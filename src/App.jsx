63caf97e4c56cd1b6665c4ab821048978d8531a0
caowens <caowens555@yahoo.com>
2024-07-18 23:31:38 -0400

    Add Greeting component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Greeting() {
+  return (
+    <h2>Welcome to React!</h2>
+  );
+}
+
 function App() {
 
   return (
     <div>
       <h1>Hello, World!</h1>
+      <Greeting />
     </div>
   )
 }
